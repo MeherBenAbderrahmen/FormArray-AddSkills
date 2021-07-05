3d432990390f3c05033b21b7c7ee147daeb5d689
Ben Abderrahmen Meher <meherbenabderrahmen@gmail.com>
2021-07-05 15:39:00 +0200

    ReactiveForms&Validators AddProduct

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AjouterProduitComponent } from './ajouter-produit/ajouter-produit.component';
+import { ListeProduitComponent } from './liste-produit/liste-produit.component';
+import { ModifierProduitComponent } from './modifier-produit/modifier-produit.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full',},
+  {path: 'listProduit', component:ListeProduitComponent},
+  {path: 'ajouterProduit', component:AjouterProduitComponent},
+  {path: 'modifierProduit/:index', component:ModifierProduitComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AjouterProduitComponent } from './ajouter-produit/ajouter-produit.component';
+import { ListeProduitComponent } from './liste-produit/liste-produit.component';
+import { ModifierProduitComponent } from './modifier-produit/modifier-produit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AjouterProduitComponent,
+    ListeProduitComponent,
+    ModifierProduitComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
