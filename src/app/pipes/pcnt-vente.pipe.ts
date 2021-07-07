68c1212db3c66045110c711677d186b88c07f39a
Ben Abderrahmen Meher <meherbenabderrahmen@gmail.com>
2021-07-08 01:42:17 +0200

    calcul

--- src/app/pipes/pcnt-vente.pipe.ts
+++ src/app/pipes/pcnt-vente.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PcntVentePipe implements PipeTransform {
 
   transform(value: any, args1: any, args2: any): any {
-    var x = (100 * (parseFloat(args2))) / parseFloat(args1);
+    var x =100 - (100 * (parseFloat(args2))) / parseFloat(args1);
 
     return '%' + parseInt(x.toString());
   }
